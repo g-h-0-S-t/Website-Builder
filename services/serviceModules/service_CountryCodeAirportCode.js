2f46d0faa4b5b7b495a56ea9748457791276f823
Ribhu <ribhubiswas@gmail.com>
2019-12-26 22:59:15 +0530

    aiport to country map java file simplification

--- services/serviceModules/service_CountryCodeAirportCode.js
+++ services/serviceModules/service_CountryCodeAirportCode.js
@@ -6,7 +6,7 @@ exports.AirportcountryMap = function(app, fs, java) {
         try {
             java.runFile(javaFile, function(err, result) {
                 console.log(result.stderr);
-                messageObject = err ? { 'color': 'red', 'text': err } : (result.stderr ? { 'color': 'red', 'text': result.stderr } : { 'color': 'green', 'text': result.stdout });
+                messageObject = err ? { "color": "red", "text": err } : (result.stderr ? { "color": "red", "text": result.stderr } : { "color": "green", "text": result.stdout });
                 res.send({ statusCode: 200, message: messageObject });
             });
         } catch (err) {
